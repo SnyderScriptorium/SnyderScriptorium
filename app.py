191e5d15b1204abd4f0b5e94de8d0da32331bded
K. W, Snyder <KSnyderauthorship@gmail.com>
2026-08-03 22:15:49 -0400

    Refactor routes and add new endpoints in app.py

--- app.py
+++ app.py
@@ -10,33 +10,53 @@ app = Flask(__name__, template_folder=os.path.join(basedir, "templates"))
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
+
+
+@app.route("/K. W. Snyder-writing")
+def K-W-Snyder-Writing():
+    return render_template("K-W-Snyder-writing.html")
+
+
+@app.route("/blog")
+def theblog():
+    return render_template("blog_templates/main_blog.html")
+
+
+@app.route("/blog/curations")
+def book-curations():
+    return render_template("blog_templates/curations.html")
+
+
+@app.route("/blog templates")
+def bookreviews():
+    return render_template("blog_templates/bookreviews.html")
+
+
+@app.route("/blog/curiosity-cabinet")
+def blog_cabinet():
+    return render_template("blog_templates/curiosity_cabinet.html")
+
+
+@app.route("/store")
+def store():
+    return render_template("store.html")
+
+
+@app.route("/merch")
+def merch():
+    return render_template("merch.html")
 
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
 
 
 @app.route("/admin")
-def admin():
-  return render_template("admin.html")
-
-
-@app.route("/book-curations")
-def book_curations():
-  return render_template("book-curations.html")
-
-
-@app.route("/book-reviews")
-def book_reviews():
-  return render_template("book-reviews.html")
-
-
-@app.route("/curiosity-cabinet")
-def curiosity_cabinet():
-  return render_template("curiosity-cabinet.html")
+def admin_dashboard():
+    return render_template("admin.html")
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
