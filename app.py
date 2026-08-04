d17c569e4187a8b01449fdbbc1e173036716eb2f
K. W, Snyder <KSnyderauthorship@gmail.com>
2026-08-04 10:54:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,52 +8,52 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__, template_folder=os.path.join(basedir, "templates"))
 
 
-@app.route("/")
+@app.route("/index.html")
 def the_hearth():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template("about.html")
 
 
-@app.route("/blog")
+@app.route("/blog.html")
 def the_blog():
     return render_template("blog_templates/theblog.html")
 
 
-@app.route("/blog/curations")
+@app.route("/blog/book_curations.html")
 def book_curations():
     return render_template("blog_templates/book_curations.html")
 
 
-@app.route("/blog/reviews")
+@app.route("/blog/bookreviews.html")
 def book_reviews():
     return render_template("blog_templates/bookreviews.html")
 
 
-@app.route("/blog/curiosity-cabinet")
+@app.route("/blog/curiosity_cabinet.html")
 def curiosity_cabinet():
     return render_template("blog_templates/curiosity_cabinet.html")
 
 
-@app.route("/snyder-writing")
+@app.route("/kwsnyderwriting.html")
 def kw_snyder_writing():
     return render_template("kwsnyderwriting.html")
 
 
-@app.route("/store")
+@app.route("/store.html")
 def the_scriptorium():
     return render_template("store.html")
 
 
-@app.route("/merch")
+@app.route("/merch.html")
 def merch_shop():
     return render_template("merch.html")
 
 
-@app.route("/admin")
+@app.route("/admin.html")
 def admin_dashboard():
     return render_template("admin.html")
 
