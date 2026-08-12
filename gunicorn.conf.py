aa17e3ae2776b864308fde37a0c0f4e43f8372a6
K. W, Snyder <KSnyderauthorship@gmail.com>
2026-08-11 20:09:01 -0400

    Enforce member sign-in before private writing routes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,3 +2,5 @@ def post_worker_init(worker):
     app = worker.wsgi
     from paypal_member_routes import register_paypal_member
     register_paypal_member(app)
+    from member_auth_guard import register_member_auth_guard
+    register_member_auth_guard(app)
